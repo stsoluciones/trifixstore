745b0db480309d049c482f75413e95b2a88fce7f
Gonzalo Torres Grau <gonzalotorresgrau@gmail.com>
2026-04-17 09:52:02 -0300

    refactor: update PrismaClient initialization to use datasourceUrl

--- src/shared/lib/prisma.ts
+++ src/shared/lib/prisma.ts
@@ -7,9 +7,7 @@ const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    datasources: {
-      db: { url: process.env.DATABASE_URL },
-    },
+    datasourceUrl: process.env.DATABASE_URL,
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
